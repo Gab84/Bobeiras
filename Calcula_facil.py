b0b27d87b4e60ae604ae90f8675c7cdd5793959a
Gabriel dos Santos Rodrigues <160123915+Gab84@users.noreply.github.com>
2024-02-28 08:58:58 -0300

    Add files via upload

---
+++ Calcula_facil.py
@@ -0,0 +1,17 @@
+print('1 = (Soma) 2 = (subtração) 3 = (Multiplicação) 4 = (Divisão) 5 = (progressão) 6 = ()')
+
+
+escolha = input('Deseja Calcular oque? Escolha de 1 a 10')
+
+
+
+
+#progressão aritimetrica
+a_n = int(print('digite o valor que quer calcular'))
+a_1 = a_n
+Nq = int(print('Digite a posição que você quer descobrir'))
+Raz = int(print('Digite o valor da razão (valor que é somado na progressão)'))
+Calculo = a_1 + (Nq - 1)*Raz
+print(Calculo)
+#
+
